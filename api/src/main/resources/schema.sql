9541458109978eda549480897dd13574ee63c570
kyunkim <kyunkim@onoffmix.com>
2023-03-03 09:47:52 +0900

    add : schema.sql 파일에 notification 테이블 추가

---
+++ api/src/main/resources/schema.sql
@@ -0,0 +1,38 @@
+drop table if exists `hibernate_sequence`;
+drop table if exists `job`;
+drop table if exists notification;
+drop table if exists notification_template;
+
+create table hibernate_sequence
+(
+    next_val bigint null
+) engine=InnoDB;
+insert into hibernate_sequence values (1);
+
+create table `job` (
+    job_id bigint not null auto_increment primary key,
+    title text not null,
+    url text not null,
+    company varchar(50) not null,
+    notice_end_date varchar(50) not null,
+    created_at datetime not null default '2022-01-01 00:00:00',
+    updated_at datetime not null default '2022-01-01 00:00:00'
+) engine=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+create table if not exists notification (
+    notification_id bigint not null auto_increment primary key,
+    title text not null,
+    receiver_id bigint not null,
+    activator_id bigint not null,
+    read_at datetime null,
+    is_read boolean not null,
+    notification_type varchar(255) not null,
+    template_type varchar(255) not null,
+    notification_template_id bigint not null
+) engine=InnoDB default charset=utf8 collate=utf8_general_ci;
+
+create table if not exists notification_template (
+    notification_template_id bigint not null auto_increment primary key,
+    title text not null,
+    `body` longtext not null
+) engine=InnoDB default charset=utf8 collate=utf8_general_ci;
