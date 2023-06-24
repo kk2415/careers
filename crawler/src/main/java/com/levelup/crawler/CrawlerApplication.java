195c57e7c67d89994a5311a0f538051c23500b46
Kim kyunghee <kkh2415@naver.com>
2023-06-24 20:31:55 +0900

    add: crawler 모듈 추가

---
+++ crawler/src/main/java/com/levelup/crawler/CrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.levelup.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.levelup")
+public class CrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerApplication.class, args);
+    }
+
+}
