2a6abc517af42bb23ac553584243c23897a8a5dd
Kim kyunghee <kkh2415@naver.com>
2023-06-24 20:42:06 +0900

    fix: job 모듈 스케쥴러, SpringApplicationListener 및 각종 설정 파일 crawler 모듈로 이동

--- crawler/src/main/java/com/levelup/crawler/config/SpringApplicationListener.java
+++ crawler/src/main/java/com/levelup/crawler/config/SpringApplicationListener.java
@@ -1,6 +1,7 @@
-package com.levelup.job.config;
+package com.levelup.crawler.config;
 
 import com.levelup.job.crawler.Crawler;
+import com.levelup.job.domain.model.Job;
 import com.levelup.job.domain.service.JobService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,12 +27,12 @@ public class SpringApplicationListener implements ApplicationListener<ContextRef
     public void onApplicationEvent(ContextRefreshedEvent event) {
         log.info("Starting ContextRefreshedEvent of SpringApplicationListener");
 
-//        crawlers.forEach(crawler -> {
-//            List<JobVO> crawledJobs = crawler.crawling();
-//            jobService.saveIfAbsent(crawledJobs, crawler.getCompany());
-//
-//            List<JobVO> deleteJobs = jobService.getNotMatched(crawledJobs, crawler.getCompany());
-//            jobService.deleteAll(deleteJobs);
-//        });
+        crawlers.forEach(crawler -> {
+            List<Job> crawledJobs = crawler.crawling();
+            jobService.saveIfAbsent(crawledJobs, crawler.getCompany());
+
+            List<Job> deleteJobs = jobService.getNotMatched(crawledJobs, crawler.getCompany());
+            jobService.deleteAll(deleteJobs);
+        });
     }
 }
--- crawler/src/main/java/com/levelup/crawler/scheduler/CrawlingScheduler.java
+++ crawler/src/main/java/com/levelup/crawler/scheduler/CrawlingScheduler.java
@@ -1,4 +1,4 @@
-package com.levelup.job.scheduler;
+package com.levelup.crawler.scheduler;
 
 import com.levelup.job.crawler.Crawler;
 import com.levelup.job.domain.service.JobService;
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -21,16 +20,12 @@ public class CrawlingScheduler {
 
     @Scheduled(cron = "0 0 */1 * * *")
     public void crawlingJobs() {
-        List<Job> jobs = new ArrayList<>();
-
         crawlers.forEach(crawler -> {
             List<Job> crawledJobs = crawler.crawling();
-            List<Job> newJobs = jobService.saveIfAbsent(crawledJobs, crawler.getCompany());
+            jobService.saveIfAbsent(crawledJobs, crawler.getCompany());
 
             List<Job> notExistsJobs = jobService.getNotMatched(crawledJobs, crawler.getCompany());
             jobService.deleteAll(notExistsJobs);
-
-            jobs.addAll(newJobs);
         });
     }
 }
